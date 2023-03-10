ef5edfa187ac846798bfa01b641b1cb99ab3f972
AqueelAhmedV <aqueelvallanchira13@gmail.com>
2023-03-11 01:04:16 +0530

    Day 2: Addedd Navbar, SearchBar, VideoCard components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
-  const token = useSelector((state) => state.token);
-
-  if (!token) {
-    return <LoginForm />;
-  }
 
   return (
     <div>
-      <h1>Todo List</h1>
-      <AddTodo />
-      <TodoList />
+      <Navbar />
     </div>
   );
 };
---
+++ client/src/components/VideoCard.jsx
@@ -0,0 +1,9 @@
+
+
+export default VideoCard = () => {
+    return (
+        <div className="video-card w-350 h-240 bg-transparent px-3 py-2 m-1">
+            
+        </div>
+    );
+}
