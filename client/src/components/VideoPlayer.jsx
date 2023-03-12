172529e12b3441623f13417ec45a16719b7b5db4
AqueelAhmedV <aqueelvallanchira13@gmail.com>
2023-03-12 23:34:00 +0530

    Add margin-top to CommentBox

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -8,7 +8,7 @@ export default function VideoPlayer({ playbackId, muted=true, autoPlay=false, lo
   const [loaded, setLoaded] = useState(true);
   console.log('render')
   return (
-    <div className="video-player lg:h-[360px] md:h-[270px] sm:h-[180px] p-4">
+    <div className="video-player lg:h-[360px] md:h-[270px] sm:h-[180px] p-4 mb-4">
 
         {!loaded?<Skeleton width="100%" height="100%"/>
         :<MuxPlayer
